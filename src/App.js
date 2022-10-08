2b46b1328ac8fdac0caf527e6870656e159e0512
Reigo_Laptop <147046093@qq.com>
2022-10-08 11:34:50 +0800

    second commit

--- src/App.js
+++ src/App.js
@@ -2,29 +2,59 @@
 import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
+import React,{ useState } from "react";
+import { nanoid } from "nanoid";
+
 function App(props) {
-  const taskList = props.tasks?.map((task) => (
+  const [tasks,setTasks]=useState(props.tasks);
+  const taskList = tasks?.map((task) => (
     <Todo 
       id={task.id} 
       name={task.name} 
       completed={task.completed}
       key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
     />
     )
   );
+  const headingNoun=taskList.length!==1?'tasks':'task';
+  const headingText=`${taskList.length} ${headingNoun} remaining.`;
 
+  function addTask(name) {
+    //const newTask={id:'todo-${nanoid()}', name, completed:false};
+    const newTask = { id: `todo-${nanoid()}`, name, completed: false };
+    setTasks([...tasks,newTask]);
+    
+    alert(name);
+  }
+  function toggleTaskCompleted(id) {
+    const updateTask=tasks.map((task)=>{
+      if(id==task.id){
+        return {...task,completed:!task.completed};
+      }
+      return task;
+    });
+    setTasks(updateTask);
+  }
   
+
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <Form/>
+      <Form addTask={addTask}/>
       <div className="filters btn-group stack-exception">
+        <button
+          type="button"
+          onClick={()=>alert("Hello")}
+        >
+          Say Hi!
+        </button>
         <FilterButton/>
         <FilterButton/>
         <FilterButton/>
       </div>
       <h2 id="list-heading">
-        3 tasks remaining
+        {headingText}
       </h2>
       <ul
         role="list"
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,21 @@
-import React from "react";
-export default function Form(){
+import React,{ useState } from "react";
+export default function Form(props){
+    function handleSubmit(e){
+        e.preventDefault();
+        props.addTask(name);
+        setName('');
+        //alert("Hello,World");
+    }
+
+    function handleChange(e){
+        //console.log(e.target.value);
+        setName(e.target.value);
+    }
+
+    const [name, setName] = useState('');
+
     return(
-        <form>
+        <form onSubmit={handleSubmit}>
             <h2 className="label-wrapper">
             <label htmlFor="new-todo-input" className="label__lg">
                 What needs to be done?
@@ -13,6 +27,8 @@ export default function Form(){
                 className="input input__lg"
                 name="text"
                 autoComplete="off"
+                value={name}
+                onChange={handleChange}
             />
             <button type="submit" className="btn btn__primary btn__lg">
                 Add
